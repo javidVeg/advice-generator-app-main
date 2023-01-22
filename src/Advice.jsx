69fa98a90ce5661d66c1efeedb2c24d801cb1498
David_Martinez <javidx24@icloud.com>
2023-01-22 13:48:36 -0500

    edits to layout

--- src/Advice.jsx
+++ src/Advice.jsx
@@ -3,14 +3,15 @@ import pause from '../images/pattern-divider-desktop.svg'
 
 const Advice = () => {
     return (
-        <div className=' w-96 h-auto rounded-lg bg-Grayish-Blue'>
-            <h1 className=' text-Neon-Green font-Manrope'>Advice</h1>
-            <div className='inline-flex items-center justify-center w-full'>
-                <hr className=' w-64 h-1 my-8 bg-white border-0' />
-                <div className='absolute px-4 -translate-x-1/2  bg-red-500 left-1/2  '>
-                <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/><g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
-                </div>
-
+        <div className='h-auto w-[600px] rounded-lg bg-Dark-Grayish-Blue flex flex-col justify-center items-center '>
+            <h1 className=' text-Neon-Green font-Manrope text-[15px]'>ADVICE #123</h1>
+            <p className=' text-Light-Cyan font-Manrope text-quote w-[75%]'>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt asperiores quasi tenetur corrupti"</p>
+            <div className='flex items-center justify-center '>
+                <svg className=' m-10 ' width="444" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z" /><g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3" /><rect x="14" width="6" height="16" rx="3" /></g></g></svg>
+            </div>
+            <div className='mt-8 flex items-center justify-center w-full'>
+                <span className='w-[65px] h-[65px]  bg-Neon-Green rounded-full absolute'></span>
+                <svg className='absolute' width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733" /></svg>
             </div>
 
         </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,21 +8,25 @@ module.exports = {
     fontFamily: {
       'Manrope': ['Manrope', 'sans-serif']
     },
-    "quote": ['28px', {
-      fontWeight: '800',
-      // lineHeight: '1.2rem'
-    }],
+    fontSize: {
+      "quote": ['28px', {
+        fontWeight: '800',
+        lineHeight: '2.2rem'
+      }],
+    },
+
     extend: {
       colors: {
         'Grayish-Blue': 'hsl(217, 19%, 38%)',
+        'Dark-Grayish-Blue': 'hsl(217, 19%, 24%)',
         'Light-Cyan': 'hsl(193, 38%, 86%)',
         'Neon-Green': 'hsl(150, 100%, 66%)',
       },
-      backgroundColor: {
-        'Grayish-Blue': 'hsl(217, 19%, 38%)',
-        'Light-Cyan': 'hsl(193, 38%, 86%)',
-        'Neon-Green': 'hsl(150, 100%, 66%)',
-      }
+      // backgroundColor: {
+      //   'Grayish-Blue': 'hsl(217, 19%, 38%)',
+      //   'Light-Cyan': 'hsl(193, 38%, 86%)',
+      //   'Neon-Green': 'hsl(150, 100%, 66%)',
+      // }
     },
   },
   plugins: [],
